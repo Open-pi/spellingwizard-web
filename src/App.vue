9c13e5edb18d2b7184698f67fa8e03eb8482f89b
Maistrox9 <youtoukabri@protonmail.com>
2021-11-25 15:21:03 +0100

    Add new fonts.

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,28 @@ import HeaderSection from './components/HeaderSection.vue'
 </template>
 
 <style>
+@font-face {
+  font-family: "Raleway";
+  src: local("Raleway"),
+    url(./fonts/Raleway/Raleway-VariableFont_wght.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Inter";
+  src: local("Inter"),
+    url(./fonts/Inter/Inter-VariableFont_slnt,wght.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Metropolis";
+  src: local("Metropolis"),
+    url(./fonts/Metropolis/Metropolis-Bold.otf) format("opentype");
+}
 :root {
   background-color: #7C3AED;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* Raleway  Inter  Metropolis */
+  font-family: "Metropolis", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
