a6e2b9ec809843beed423e8555c4b2c8bd413c28
Maistrox9 <youtoukabri@protonmail.com>
2021-11-26 16:42:28 +0100

    Reformat the privacy policy and terms of use cards.

--- src/components/legals/TermsOfUseSection.vue
+++ src/components/legals/TermsOfUseSection.vue
@@ -8,7 +8,7 @@
         <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
           Terms &amp; Conditions
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             By downloading or using the app, these terms will
                   automatically apply to you – you should make sure therefore
                   that you read them carefully before using the app. You’re not
@@ -20,7 +20,7 @@
                   database rights, and other intellectual property rights related
                   to it, still belong to Open Pi.
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             Open Pi is committed to ensuring that the app is
                   as useful and efficient as possible. For that reason, we
                   reserve the right to make changes to the app or to charge for
@@ -28,7 +28,7 @@
                   charge you for the app or its services without making it very
                   clear to you exactly what you’re paying for.
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             The Spelling Wizard app stores and processes personal data that
                   you have provided to us, to provide our
                   Service. It’s your responsibility to keep your phone and
@@ -41,34 +41,34 @@
                   that the Spelling Wizard app won’t work properly or at all.
         </p>
         <div>
-            <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+            <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
                 The app does use third-party services that declare their
                     Terms and Conditions.
             </p>
-            <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+            <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
                 Link to Terms and Conditions of third-party service
                     providers used by the app
             </p>
-            <ul>
+            <ul class="text-left mt-4 max-w-4xl lg:mx-auto list-disc">
                 <li>
-                    <a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer">
+                    <a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer" class="text-blue-600 no-underline hover:underline">
                         Google Play Services
                     </a>
                 </li>
                 <li>
-                    <a href="https://developers.google.com/admob/terms" target="_blank" rel="noopener noreferrer">
+                    <a href="https://developers.google.com/admob/terms" target="_blank" rel="noopener noreferrer" class="text-blue-600 no-underline hover:underline">
                         AdMob
                     </a>
                 </li>
                 <li>
-                    <a href="https://firebase.google.com/terms/analytics" target="_blank" rel="noopener noreferrer">
+                    <a href="https://firebase.google.com/terms/analytics" target="_blank" rel="noopener noreferrer" class="text-blue-600 no-underline hover:underline">
                         Google Analytics for Firebase
                     </a>
                 </li>
                 <!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!---->
             </ul>
         </div>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             You should be aware that there are certain things that
                   Open Pi will not take responsibility for. Certain
                   functions of the app will require the app to have an active
@@ -79,7 +79,7 @@
                   have any of your data allowance left.
         </p>
         <p></p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             If you’re using the app outside of an area with Wi-Fi, you
                   should remember that the terms of the agreement with your
                   mobile network provider will still apply. As a result, you may
@@ -93,14 +93,14 @@
                   using the app, please be aware that we assume that you have
                   received permission from the bill payer for using the app.
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             Along the same lines, Open Pi cannot always take
                   responsibility for the way you use the app i.e. You need to
                   make sure that your device stays charged – if it runs out of
                   battery and you can’t turn it on to avail the Service,
                   Open Pi cannot accept responsibility.
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             With respect to Open Pi’s responsibility for your
                   use of the app, when you’re using the app, it’s important to
                   bear in mind that although we endeavor to ensure that it is
@@ -110,7 +110,7 @@
                   loss, direct or indirect, you experience as a result of
                   relying wholly on this functionality of the app.
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             At some point, we may wish to update the app. The app is
                   currently available on Android &amp; iOS – the requirements for the 
                   both systems(and for any additional systems we
@@ -128,28 +128,28 @@
                   (b) you must stop using the app, and (if needed) delete it
                   from your device.
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
-            <strong>Changes to This Terms and Conditions</strong>
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+            <strong class="text-gray-900">Changes to This Terms and Conditions</strong>
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             We may update our Terms and Conditions
                   from time to time. Thus, you are advised to review this page
                   periodically for any changes. We will
                   notify you of any changes by posting the new Terms and
                   Conditions on this page.
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             These terms and conditions are effective as of 2021-11-25
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
-            <strong>Contact Us</strong>
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+            <strong class="text-gray-900">Contact Us</strong>
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             If you have any questions or suggestions about our
                   Terms and Conditions, do not hesitate to contact us
                   at OpenPi@protonmail.com.
         </p>
-        <p class="mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
+        <p class="text-left mt-4 max-w-5xl text-xl text-gray-500 lg:mx-auto">
             This Terms and Conditions page was generated by <a href="https://app-privacy-policy-generator.nisrulz.com/" target="_blank" rel="noopener noreferrer">App Privacy Policy Generator</a>
         </p>
       </div>
